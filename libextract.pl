6a41c2beea985a37fc3f5531fccdd26be96bf604
Kyle Banerjee <kyle.banerjee@gmail.com>
2014-12-08 16:24:11 -0800

    Include failed OCR code but add in a bit of garbage cleanup

--- libextract.pl
+++ libextract.pl
@@ -25,7 +25,8 @@ if ($alchemyObj->LoadKey("api_key.txt") eq "error")
 my $result = '';
 my $doc = '';
 my $url = '';
-my $filename = 'temp_pdf';
+my $pdf = '';
+my $filename = 'temp.pdf';
 
 
 my $entityParams = new AlchemyAPI_EntityParams();
@@ -33,27 +34,38 @@ $entityParams->SetDisambiguate(0);
 $entityParams->SetLinkedData(0);
 $entityParams->SetOutputMode('xml');
 
-$url = 'http://digitalcommons.ohsu.edu/cgi/viewcontent.cgi?article=1000&context=hca-oralhist'; 
+$url = 'http://digitalcommons.ohsu.edu/cgi/viewcontent.cgi?article=1020&context=hca-oralhist'; # textual
+#$url = 'http://digitalcommons.ohsu.edu/cgi/viewcontent.cgi?article=2108&context=etd'; # image pdf 
 
 my $content = get($url);
-die "Couldn't get it!" unless defined $content;
+die "Couldn't download page" unless defined $content;
 
 open(PDFFILE, '>', $filename);
 print PDFFILE $content;
 close PDFFILE;
 
-
-my $pdf = CAM::PDF->new($filename);
+$pdf = CAM::PDF->new($filename);
 
 for (my $i=1; $i <= $pdf->numPages(); $i++) {
 	$doc = $doc.$pdf->getPageText($i);
 	}
+
+#get rid of junk
+$doc =~ s/[^!-~\s]//g; 
+# get rid of repeated whitespace
+$doc =~ s/([ \n\r])+/$1/gi; 
+
+#$pdf = PDF::OCR2->new($filename);
+#$PDF::OCR2::CHECK_PDF   = 0;
+#$PDF::OCR2::REPAIR_XREF = 1;
+#$doc = $pdf->text;
 
 # Get a ranked list of named entities for a web URL.
 #$result = $alchemyObj->URLGetRawText("http://nwda.orbiscascade.org/ark:/80444/xv08340");
 #$result = $alchemyObj->URLGetRankedNamedEntities("http://nwda.orbiscascade.org/ark:/80444/xv08340", $entityParams);
 #$result = $alchemyObj->URLGetRankedNamedEntities("http://digitalcommons.ohsu.edu/fdadrug/", $entityParams);
 $result = $alchemyObj->TextGetRankedNamedEntities($doc, $entityParams);
+
 if ($result ne "error")
 {
 	printf $result;
